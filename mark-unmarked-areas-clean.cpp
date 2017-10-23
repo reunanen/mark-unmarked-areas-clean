7107d9be0655ec5dab5fdb09ad6c41054aa6eda0
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-10-23 15:54:19 +0300

    Generate a clean mask when none exists

--- mark-unmarked-areas-clean.cpp
+++ mark-unmarked-areas-clean.cpp
@@ -12,11 +12,22 @@ int main(int argc, char** argv)
 
     const std::string mask_image_suffix = "_mask.png";
 
-    std::cout << "Searching for mask images..." << std::endl;
+    std::cout << "Searching for images..." << std::endl;
 
-    const std::vector<dlib::file> files = dlib::get_files_in_directory_tree(argv[1], dlib::match_ending(mask_image_suffix));
+    const std::vector<dlib::file> files = dlib::get_files_in_directory_tree(argv[1],
+        [](const dlib::file& name) {
+            if (dlib::match_ending("_mask.png")(name)) {
+                return false;
+            }
+            if (dlib::match_ending("_result.png")(name)) {
+                return false;
+            }
+            return dlib::match_ending(".jpeg")(name)
+                || dlib::match_ending(".jpg")(name)
+                || dlib::match_ending(".png")(name);
+        });
 
-    std::cout << "Processing " << files.size() << " mask images..." << std::endl;
+    std::cout << "Processing " << files.size() << " images..." << std::endl;
 
     cv::Vec4b ignore_color(0, 0, 0, 0);
     cv::Vec4b clean_color(0, 255, 0, 64);
@@ -27,29 +38,39 @@ int main(int argc, char** argv)
 
         std::cout << "Processing " << full_name;
 
-        cv::Mat mask_image = cv::imread(full_name, cv::IMREAD_UNCHANGED);
+        const std::string label_filename = full_name + "_mask.png";
 
-        std::cout
-            << ", width = " << mask_image.cols
-            << ", height = " << mask_image.rows
-            << ", channels = " << mask_image.channels()
-            << ", type = 0x" << std::hex << mask_image.type();
+        cv::Mat mask_image = cv::imread(label_filename, cv::IMREAD_UNCHANGED);
 
-        size_t converted_pixel_count = 0;
-        for (int y = 0; y < mask_image.rows; ++y) {
-            cv::Vec4b* row = mask_image.ptr<cv::Vec4b>(y);
-            for (int x = 0; x < mask_image.cols; ++x) {
-                cv::Vec4b& color = row[x];
-                if (color == ignore_color) {
-                    color = clean_color;
-                    ++converted_pixel_count;
+        if (!mask_image.data) {
+            cv::Mat input_image = cv::imread(full_name);
+            mask_image.create(input_image.size(), CV_8UC4);
+            mask_image.setTo(clean_color);
+            std::cout << ": generated a clean mask";
+        }
+        else {
+            std::cout
+                << ", width = " << mask_image.cols
+                << ", height = " << mask_image.rows
+                << ", channels = " << mask_image.channels()
+                << ", type = 0x" << std::hex << mask_image.type();
+
+            size_t converted_pixel_count = 0;
+            for (int y = 0; y < mask_image.rows; ++y) {
+                cv::Vec4b* row = mask_image.ptr<cv::Vec4b>(y);
+                for (int x = 0; x < mask_image.cols; ++x) {
+                    cv::Vec4b& color = row[x];
+                    if (color == ignore_color) {
+                        color = clean_color;
+                        ++converted_pixel_count;
+                    }
                 }
             }
-        }
 
-        std::cout << ": converted " << std::dec << converted_pixel_count << " pixels";
+            std::cout << ": converted " << std::dec << converted_pixel_count << " pixels";
+        }
         
-        cv::imwrite(full_name, mask_image);
+        cv::imwrite(label_filename, mask_image);
         
         std::cout << std::endl;
     }
